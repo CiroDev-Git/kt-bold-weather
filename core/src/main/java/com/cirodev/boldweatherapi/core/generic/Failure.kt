012cbb079ea7e4c920415bea91b72a11a518bc5e
CiroDev <cristian_ciro@outlook.es>
2023-12-05 11:49:55 -0500

    Compose, Kotlin & JVM gradle issues resolved

--- core/src/main/java/com/cirodev/boldweatherapi/core/generic/Failure.kt
+++ core/src/main/java/com/cirodev/boldweatherapi/core/generic/Failure.kt
@@ -1,6 +1,6 @@
 package com.cirodev.boldweatherapi.core.generic
 
 sealed class Failure {
-    data object NetworkError : Failure()
-    data object UnknownError : Failure()
+    object NetworkError : Failure()
+    object UnknownError : Failure()
 }
